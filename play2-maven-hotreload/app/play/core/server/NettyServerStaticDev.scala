7684eb7923c3744dcbac306df21bfe4dc56e424a
Jeroen <jeroen@admins-MacBook-Pro-4.local>
2015-05-17 05:06:59 -0400

    added documentation app to netty server

--- play2-maven-hotreload/app/play/core/server/NettyServerStaticDev.scala
+++ play2-maven-hotreload/app/play/core/server/NettyServerStaticDev.scala
@@ -7,6 +7,8 @@ import play.core.ApplicationProvider
 import play.api.DefaultApplication
 import play.api.Play
 import scala.util.Success
+import play.docs.BuildDocHandlerFactory
+import java.util.jar.JarFile
 
 /**
  * Creates a DefaultApplication in Dev mode wrapped by a Static Application Provider and passes it to NettyServer.
@@ -27,7 +29,19 @@ object NettyServerStaticDev {
 			} else {
 				play.utils.Threads.withContextClassLoader(this.getClass.getClassLoader) {
 					try {
+						// Locate play-docs jar file on the class path
+						val jarFile = System.getProperty("java.class.path").split(":").map(new java.io.File(_))
+										.find(_.getName startsWith "play-docs")
+										.map(new JarFile(_))
+						val buildDocHandler = jarFile map { BuildDocHandlerFactory.fromJar(_, "play/docs/content") }
+						
 						val appProvider = new ApplicationProvider {
+							
+							override def handleWebCommand(request: play.api.mvc.RequestHeader): Option[play.api.mvc.Result] = {
+								// if play-docs jar file is on the class path, re-route documentation request to buildDocHandler
+								buildDocHandler flatMap { _.maybeHandleDocRequest(request).asInstanceOf[Option[play.api.mvc.Result]] }
+							}
+							
 							val application = new DefaultApplication(
 								applicationFile,
 								this.getClass.getClassLoader,
